f17ed4f2ac2fc87018c4ea11e17a4cbec2f367f4
Supriya <supriyasingh2105@gmail.com>
2024-02-28 04:34:11 +0530

    First commit

--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
-import inquirer from 'inquirer';
-import qr from 'qr-image' ;
+import express from "express";
+import bodyParser from "body-parser";
+import qr from 'qr-image';
 import fs from 'fs';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-inquirer
-  .prompt([
-    {message:"Type ur url",
-     name:"url",
-    },
-  ])
-  .then((answers) => {
-    const Url = answers.url;
-    var qr_svg = qr.image('Url');
-    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
 
-    fs.writeFile('url.txt', Url, (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
-      }); 
-    })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+// Initialize the text and image filenames
+const textFilename = __dirname + "/public/url.txt";
+const imageFilename = __dirname + "/public/qr_img.png";
+
+// Check if the text file exists, if not, create it
+if (!fs.existsSync(textFilename)) {
+  fs.writeFileSync(textFilename, '');
+}
+
+app.post("/submit", (req, res) => {
+  const input_text = req.body["qr-generator"];
+
+  // Save input text to the text file
+  fs.writeFile(__dirname + `/public/url.txt`, input_text, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
   });
 
+  // Generate QR code using the input text and overwrite the existing image file
+  const qr_svg = qr.image(input_text, { type: 'png' });
+  qr_svg.pipe(fs.createWriteStream(imageFilename));
 
- 
+  res.send(`<h1>QR Generator</h1><img src="/qr_img.png" alt="QR Code">`);
+});
 
- 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
